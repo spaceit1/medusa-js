701958f1557de24697883c63e386035a14af9a2f
Pawel Boron <pawcio2001.boron@interia.pl>
2024-10-31 10:50:23 +0100

    Poprawa commonjs w migracji

--- apps/backend/src/modules/b2b-accounts/migrations/Migration20241031090918.ts
+++ apps/backend/src/modules/b2b-accounts/migrations/Migration20241031090918.ts
@@ -1,11 +1,15 @@
-const { Migration } = require('@mikro-orm/migrations');
+import { Migration } from "@mikro-orm/migrations";
 
-module.exports = class Migration20241031085141 extends Migration {
-  async up() {
-    this.addSql('ALTER TABLE "customer" ADD COLUMN IF NOT EXISTS "approved" boolean NOT NULL DEFAULT false;');
+export class Migration20241031090918 extends Migration {
+  async up(): Promise<void> {
+    this.addSql(
+      'ALTER TABLE "customer" ADD COLUMN IF NOT EXISTS "approved" boolean NOT NULL DEFAULT false;'
+    );
   }
 
-  async down() {
-    this.addSql('ALTER TABLE "customer" DROP COLUMN IF EXISTS "approved";');
+  async down(): Promise<void> {
+    this.addSql(
+     'ALTER TABLE "customer" DROP COLUMN IF EXISTS "approved";'
+    );
   }
 }
