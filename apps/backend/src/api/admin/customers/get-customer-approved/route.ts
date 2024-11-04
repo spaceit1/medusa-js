ec9c72f28c8300167700ea3ad6feaaaac6b8d948
Pawel Boron <pawcio2001.boron@interia.pl>
2024-11-04 11:05:42 +0100

    Sprawdzanie czy uzytkownik ma approved

---
+++ apps/backend/src/api/admin/customers/get-customer-approved/route.ts
@@ -0,0 +1,25 @@
+import { MedusaRequest, MedusaResponse } from "@medusajs/framework/http";
+import { Pool } from "pg";
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+const corsOptions = {
+    origin: process.env.STORE_CORS,
+    credentials: false,
+};
+
+export const GET = async (req: any, res: any) => {
+    res.status(200).json({ message: "Hello, Medusa!" });
+};
+
+// Dodanie obsługi OPTIONS dla preflight requests
+export const OPTIONS = cors(corsOptions);
+
+// Wyłączenie domyślnej obsługi CORS Medusa
+export const CORS = false;
