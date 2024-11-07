3c1dd958abef69a962ff1bb5ae610ad01f0c008c
spaceit1 <biuro@spaceit.com.pl>
2024-11-07 07:30:17 +0100

    Usunięcie nieużywanych interfejsów DTO w module załączników

--- apps/backend/src/modules/attachment/types/index.ts
+++ apps/backend/src/modules/attachment/types/index.ts
@@ -22,31 +22,6 @@ export type ModuleAttachment = {
    deleted_at: Date | null;
  };
 
-export interface AttachmentDTO {
-   id: string;
-   name: string;
-   file_name: string;
-   language: Language;
-   type: Type;
-   created_at: Date;
-   updated_at: Date;
-   deleted_at: Date | null;
-}
-
-export interface AttachmentToProductDTO {
-   id: string;
-   attachment_id: string;
-   product_id: string;
-   attachment?: AttachmentDTO;
-   product?: ProductDTO;
-   created_at: Date;
-   updated_at: Date;
-}
-
-export interface ProductWithAttachmentsDTO extends ProductDTO {
-   attachments?: AttachmentDTO[];
-}  
-
 export type AttachmentData = InferTypeOf<typeof Attachment>;
 
 export interface ModuleUpdateAttachment extends Partial<ModuleAttachment> {
